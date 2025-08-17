6e361371425a9b7cad27b88d88f3ab1f9d1e1390
kzwetz26 <111131228+kzwetz26@users.noreply.github.com>
2025-08-17 18:55:39 -0500

    lightbox prototype

---
+++ javascript/lightbox.js
@@ -0,0 +1,16 @@
+const lightbox = document.getElementById("lightbox");
+const image = document.querySelector(".lightbox-img");
+const close = document.querySelector(".lightbox .close");
+
+document.querySelectorAll(".image").forEach(item => {
+    item.addEventListener("click", () => {
+        const img = item.querySelector("img");
+        lightbox.style.display = "flex";
+        image.src = img.src;
+        image.alt = img.alt;
+    });
+});
+
+close.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
